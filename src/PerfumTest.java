f6e09be22b6e0477d11c2a0450d3b4c13f4c0130
rudyknowak <rudyknowak@gmail.com>
2018-02-14 19:28:11 +0100

    poprawka 1

--- src/PerfumTest.java
+++ src/PerfumTest.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
-
 public class PerfumTest {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         Perfum[] data = new Perfum[3];
 
-        for (int i = 0; i < data.length; i++) {
-            data[i] = new Perfum();
-            System.out.println("Podaj nazwę perfum");
-            data[i].setName(input.nextLine());
-            System.out.println("Podaj cenę perfum");
-            data[i].setPrice(input.nextInt());
-            input.nextLine();
-            if (data[0].equals(data[1])){
-                System.out.println("Taki produkt już istnieje w bazie-nowy produkt nie został utworzony");
-            } else {
-                System.out.println("Nowy produkt o nazwie " + data[i].getName() + " i cenie " + data[i].getPrice() + " został utworzony");
+        int index = 0;
+        Perfum temp = new Perfum();
+
+        for (int i = 0; i <data.length ; i++) {
+            do {
+                System.out.println("Podaj nazwę perfum");
+                temp.setName(input.nextLine());
+                System.out.println("Podaj cenę perfum");
+                temp.setPrice(input.nextInt());
+                input.nextLine();
+
+                if (!temp.equals(data[index])) {
+                    data[index] = temp;
+                    System.out.println("Nowy produkt o nazwie " + data[index].getName() + " i cenie "
+                            + data[index].getPrice() + " został utworzony");
+                } else {
+                    System.out.println("Taki produkt już istnieje, podaj inny");
+                }
             }
+            while (temp.equals(data[index]));
+            index++;
         }
     }
 }
-
