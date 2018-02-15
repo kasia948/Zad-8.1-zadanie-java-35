544976d957631a6281eed04af28ef2067f047f5a
rudyknowak <rudyknowak@gmail.com>
2018-02-15 22:15:15 +0100

    poprawka 2

--- src/PerfumTest.java
+++ src/PerfumTest.java
@@ -1,31 +1,30 @@
 import java.util.Scanner;
+
 public class PerfumTest {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         Perfum[] data = new Perfum[3];
 
-        int index = 0;
-        Perfum temp = new Perfum();
+        while (data[data.length-1] == null) { // dopóki ostatni element tablicy nie jest nullem (while)
+            Perfum temp = new Perfum();
+            System.out.println("Podaj nazwę perfum");
+            temp.setName(input.nextLine());
+            System.out.println("Podaj cenę perfum");
+            temp.setPrice(input.nextInt());
+            input.nextLine();
 
-        for (int i = 0; i <data.length ; i++) {
-            do {
-                System.out.println("Podaj nazwę perfum");
-                temp.setName(input.nextLine());
-                System.out.println("Podaj cenę perfum");
-                temp.setPrice(input.nextInt());
-                input.nextLine();
-
-                if (!temp.equals(data[index])) {
-                    data[index] = temp;
-                    System.out.println("Nowy produkt o nazwie " + data[index].getName() + " i cenie "
-                            + data[index].getPrice() + " został utworzony");
-                } else {
+            for (int j = 0; j < data.length; j++) {
+                if (!temp.equals(data[j]) && data[j] == null) {
+                    data[j] = temp;
+                    System.out.println("Nowy produkt o nazwie " + data[j].getName() + " i cenie "
+                            + data[j].getPrice() + " został utworzony");
+                    break;
+                } else if (temp.equals(data[j])) {
                     System.out.println("Taki produkt już istnieje, podaj inny");
+                    break;
                 }
             }
-            while (temp.equals(data[index]));
-            index++;
         }
     }
 }
